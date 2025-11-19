be3ee3434eec411cd435c2c0433fc3b4e4431336
amantiwari2357 <amankumartiwari5255@gmail.com>
2025-11-19 18:17:18 +0530

    added api

--- devfolio-marketplace/src/services/api.ts
+++ devfolio-marketplace/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend API URL
+  baseURL: 'https://devfolio-marketplace-1.onrender.com/api', // Backend API URL
   headers: {
     'Content-Type': 'application/json',
   },
